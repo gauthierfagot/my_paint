ca6e522146be1975a399dda4a5597c95ff0d8a8c
Idriss Akbi <idriss.akbi@epitech.eu>
2024-02-25 17:30:05 +0100

    [ADD] Setting up basis

---
+++ include/my_paint.h
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2024
+** paint.h
+** File description:
+** paint.h
+*/
+
+#ifndef PAINT
+    #define PAINT
+    #include <stddef.h>
+    #include <stdlib.h>
+    #include <stdbool.h>
+    #include <SFML/Audio.h>
+    #include <SFML/Graphics.h>
+
+#endif /* !PAINT */
---
+++ src/main.c
@@ -0,0 +1,11 @@
+/*
+** EPITECH PROJECT, 2024
+** main.c
+** File description:
+** main
+*/
+
+int main(void)
+{
+    return 84;
+}
