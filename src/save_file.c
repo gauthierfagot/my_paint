b7e3a78403e3f5f6c7be7c77587a0ef779a30010
FireSky33 <gauthier.fagot@epitech.eu>
2024-03-15 02:56:14 +0100

    [FIX]: save file function

--- src/save_file.c
+++ src/save_file.c
@@ -18,17 +18,41 @@ sfBool check_format(char *file_name)
     return sfTrue;
 }
 
+char *concat_str(char const *str1, char const *str2)
+{
+    int len_str1 = my_strlen(str1);
+    int len_str2 = my_strlen(str2);
+    char *new_str = malloc(sizeof(char) * (len_str1 +
+    len_str2 + 1));
+
+    if (new_str == NULL)
+        return NULL;
+    for (int i = 0; i < len_str1; i ++)
+        new_str[i] = str1[i];
+    for (int j = 0; j < len_str2; j ++)
+        new_str[len_str1 + j] = str2[j];
+    new_str[len_str1 + len_str2] = '\0';
+    return new_str;
+}
+
 sfBool create_file_name(paint_t *paint, char *input, sfImage *new_image)
 {
     char *file_name = my_strndup(input, my_strlen(input) - 1);
+    char *file_name_jpg = NULL;
 
     if (file_name == NULL)
         return sfFalse;
-    if (check_format(file_name))
-        file_name = my_strcat(file_name, ".jpg");
-    sfImage_copyImage(new_image, paint->image, 0, 0, (sfIntRect){}, sfTrue);
-    sfImage_saveToFile(new_image, file_name);
-    sfImage_destroy(new_image);
+    if (check_format(file_name)) {
+        file_name_jpg = concat_str(file_name, ".jpg");
+        sfImage_copyImage(new_image, paint->image, 0, 0,
+        (sfIntRect){}, sfTrue);
+        sfImage_saveToFile(new_image, file_name_jpg);
+        free(file_name_jpg);
+    } else {
+        sfImage_copyImage(new_image, paint->image, 0, 0,
+        (sfIntRect){}, sfTrue);
+        sfImage_saveToFile(new_image, file_name);
+    }
     free(file_name);
     return sfTrue;
 }
@@ -51,4 +75,5 @@ void save_drawing(void *data, graphical_tool_t *)
     if (create_file_name(paint, input, new_image) == sfFalse)
         my_puterr("Failed to save image\n");
     free(input);
+    sfImage_destroy(new_image);
 }
