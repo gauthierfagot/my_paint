686caeb7baecb47f1dcae9af803def074d85d4eb
Idriss Akbi <idriss.akbi@epitech.eu>
2024-03-15 18:41:12 +0100

    [MODIFIED] 1- Destroying image after importing

--- src/button_actions/import_file.c
+++ src/button_actions/import_file.c
@@ -10,6 +10,22 @@
 #include "define.h"
 #include <stdio.h>
 
+void import_file(char *line, char *filename, sfImage *image, paint_t *paint)
+{
+    if (line != NULL) {
+        filename = clear_str(line);
+        free(line);
+        image = sfImage_createFromFile(filename);
+        if (filename != NULL)
+            free(filename);
+        if (image == NULL)
+            return;
+        sfImage_copyImage(paint->image, image, 0, 0, (sfIntRect){0}, sfTrue);
+        sfImage_destroy(image);
+    }
+    return;
+}
+
 void import_image(void *arg, graphical_tool_t *)
 {
     paint_t *paint = (paint_t *)arg;
@@ -20,16 +36,7 @@ void import_image(void *arg, graphical_tool_t *)
 
     write(1, "Choose a file to load: ", 23);
     getline(&line, &nb, stdin);
-    if (line != NULL) {
-        filename = clear_str(line);
-        free(line);
-        image = sfImage_createFromFile(filename);
-        if (filename != NULL)
-            free(filename);
-        if (image == NULL)
-            return;
-        sfImage_copyImage(paint->image, image, 0, 0, (sfIntRect){0}, sfTrue);
-    }
+    import_file(line, filename, image, paint);
     write(1, "Successfully loaded\n", 20);
     return;
 }
